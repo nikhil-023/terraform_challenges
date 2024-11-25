26154d9f0165c906f228e75d4c3f302090b99df2
nikhil-023 <118956482+nikhil-023@users.noreply.github.com>
2024-11-25 16:44:25 +0530

    Delete kplabs-terraform-challenges-main directory

--- kplabs-terraform-challenges-main/challenge-3/main.tf
+++
@@ -1,9 +0,0 @@
-
-variable "instance_config" {
-  type = map
-  default = {
-    instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
-    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
-  }
-}
-
